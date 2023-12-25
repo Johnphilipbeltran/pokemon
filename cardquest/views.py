65ba92e59dea015d1f0f56b56e1e75f77b9d6ebf
Johnphilipbeltran <141928294+Johnphilipbeltran@users.noreply.github.com>
2023-12-25 14:14:49 +0800

    Update views.py

--- cardquest/views.py
+++ cardquest/views.py
@@ -48,7 +48,7 @@ class PokemonCardListView(ListView):
     model = PokemonCard
     context_object_name = 'pokemoncard'
     template_name = 'pokemoncard.html'
-    paginate_by = 6
+    paginate_by = 5
     json_file_path = 'projectsite\data\pokemon_data.json'
 
     def get_context_data(self, **kwargs):
